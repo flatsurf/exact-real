964b08a140ecea7b1ae76e2a148bbe0248704a00
Julian Rüth <julian.rueth@fsfe.org>
2019-04-26 01:34:43 +0200

    Coveralls should report for master

--- recipe/test-coverage.sh
+++ recipe/test-coverage.sh
@@ -13,5 +13,6 @@ unset PIP_NO_DEPENDENCIES
 pip install cpp-coveralls
 
 cd $SRC_DIR/src/libexactreal/
+git checkout -b master
 set +x
 if [ ${#COVERALLS_REPO_TOKEN} = 33 ];then coveralls --gcov `which x86_64-conda_cos6-linux-gnu-gcov` --exclude exact-real/external --gcov-options '\-lrp' -b .; fi
