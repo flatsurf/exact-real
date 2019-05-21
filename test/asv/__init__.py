3bca4615449e30c270fba94acff23efb9eeefec6
Julian Rüth <julian.rueth@fsfe.org>
2019-05-21 16:38:32 +0200

    Fix ASV output

--- test/asv/__init__.py
+++ test/asv/__init__.py
@@ -1,3 +1,31 @@
+#*********************************************************************
+#  This file is part of flatsurf.
+#
+#        Copyright (C) 2019 Julian Rüth
+#
+#  Flatsurf is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  Flatsurf is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with flatsurf. If not, see <https://www.gnu.org/licenses/>.
+#*********************************************************************
+
+# This module turn google (C++) benchmarks into a Python module that can be run
+# by AirSpeedVelocity which provides a nice web interface to see how benchmarks
+# perform over time.
+
+# If you want to use this module, make sure NOT to add the conda-forge package
+# 'benchmark' (google benchmark) to your asv.conf.json matrix. The benchmark
+# key is used internally by asv. Instead, add "conda install -c conda-forge
+# benchmark" to your build_commands.
+
 import json
 import glob
 import os
@@ -24,7 +52,7 @@ for benchmark in glob.glob('test/**/*.*.cc'):
         time = float(bench["cpu_time"])
         unit = bench["time_unit"]
 
-        asv_name = "%s%s"%(PREFIX, name.replace("/", "_"), )
+        asv_name = "%s%s"%(PREFIX, name.replace("/", "_").replace("<", "_").replace(">", "_"), )
         def asv_method(self, t=time): return t
         asv_method.unit = unit
         asv_method.number = 1
