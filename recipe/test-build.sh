be859cc6b851cd33340f6cc4355c5c290fb38705
Julian Rüth <julian.rueth@fsfe.org>
2019-07-10 01:18:22 +0200

    Show distcheck errors in CI

--- recipe/test-build.sh
+++ recipe/test-build.sh
@@ -7,7 +7,8 @@ make check || (cat `find -name test-suite.log`; false)
 ./recipe/test-asv.sh
 
 # Finally some static checks
-LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH" ./recipe/build-distcheck.sh
+LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH" ./recipe/build-distcheck.sh || (cat `find -name test-suite.log`; false)
+
 # We don't run static analysis at the moment. cppcheck does not fully support
 # C++17, and clang-tidy is not packaged for conda-forge yet.
 # ./recipe/build-clang-tidy.sh
