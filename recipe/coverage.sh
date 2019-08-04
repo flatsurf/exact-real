6d9de83e70567e136da28de7669cdc9571ee9c20
Julian Rüth <julian.rueth@fsfe.org>
2019-08-04 17:52:10 +0200

    fix reporting of header files in codecov

--- recipe/coverage.sh
+++ recipe/coverage.sh
@@ -7,6 +7,8 @@ make check
 # Install gcov
 conda install -y --quiet gcc_linux-64
 
+# gcov should be invoked in the directory where the compiler was invorked
+# originally, i.e., in the place where the Makefile resides
 cd $SRC_DIR/libexactreal/src
 
 git remote remove origin || true
@@ -16,6 +18,11 @@ git checkout -b master
 git branch -u origin/master
 
 set +x
+# Do not upload; print what would have been uploaded instead…
+COVERALLS_FLAGS="-d"
 if [ ${#COVERALLS_REPO_TOKEN} = 36 ];then
-  bash <(curl -s https://codecov.io/bash) -t $COVERALLS_REPO_TOKEN -x `which x86_64-conda_cos6-linux-gnu-gcov` -a '\-lrp' -p .
+  # … unless we have the token, then actually upload.
+  COVERALLS_FLAGS="-t $COVERALLS_REPO_TOKEN"
 fi
+
+bash <(curl -s https://codecov.io/bash) $COVERALLS_FLAGS -R `pwd` -x `which x86_64-conda_cos6-linux-gnu-gcov` -a '\-lrp' -p .
