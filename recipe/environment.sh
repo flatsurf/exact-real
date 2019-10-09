431d773870400949cbf2b5b9857bbe028a1e15ea
Julian Rüth <julian.rueth@fsfe.org>
2019-10-09 02:55:40 +0200

    cleanup variables

--- recipe/environment.sh
+++ recipe/environment.sh
@@ -5,10 +5,9 @@
 # conda-build environment.
 # ##################################################################
 
-export SNIPPETS_DIR=${SNIPPETS_DIR:-$SRC_DIR/recipe/snippets}
+export SNIPPETS_DIR=`realpath "${SNIPPETS_DIR:-$SRC_DIR/recipe/snippets}"`
 export GITHUB_ORGANIZATION=flatsurf
 export GITHUB_REPOSITORY=exact-real
-export build_flavour=${build_flavour:-release}
 
 source $SNIPPETS_DIR/cxx/environment.sh
 source $SNIPPETS_DIR/make/environment.sh
