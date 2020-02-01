1c1c9b6cf62e34ebe3640ecc6861837cf5d40c21
Julian Rüth <julian.rueth@fsfe.org>
2020-02-01 22:53:40 +0100

    Fix license headers

--- pyexactreal/src/pyexactreal/exact_reals.py
+++ pyexactreal/src/pyexactreal/exact_reals.py
@@ -37,7 +37,7 @@ The module is automatically enlarged as needed::
 #
 #  exact-real is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
+#  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  exact-real is distributed in the hope that it will be useful,
