a681c57e0896fb3101a8a0afbdc1580967454b31
Julian Rüth <julian.rueth@fsfe.org>
2022-01-22 22:27:01 -0600

    Fix project name in cppasv helper

--- tools/asv/__init__.py
+++ tools/asv/__init__.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 #*********************************************************************
-#  This file is part of intervalxt.
+#  This file is part of exact-real.
 #
-#        Copyright (C) 2019-2020 Julian Rüth
+#        Copyright (C) 2019-2022 Julian Rüth
 #
-#  intervalxt is free software: you can redistribute it and/or modify
+#  exact-real is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  intervalxt is distributed in the hope that it will be useful,
+#  exact-real is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with intervalxt. If not, see <https://www.gnu.org/licenses/>.
+#  along with exact-real. If not, see <https://www.gnu.org/licenses/>.
 #*********************************************************************
 
 import os
